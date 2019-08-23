50ce91b9364195560f21b75e9d69cd22b4036d4f
Yumi Ta <soraishi08@gmail.com>
2019-08-23 02:52:51 -0400

    Add 8ball command.

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+import random
+
 client = commands.Bot(command_prefix = '.')
 
 @client.event
@@ -10,6 +12,11 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f'{round(client.latency * 1000)}ms')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx):
+    responses = ['Definitely.', 'Perhaps.', 'Definitely not.']
+    await ctx.send(random.choice(responses))
 
 client.run('NTg0ODE3NTMxNDU0MjI2NTAz.XV-Awg.hKgssTLIdHwiVK7mqI7K5clB6ew')
 
