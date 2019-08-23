4dbefa8715dba00d3ded2a898d66ec5550e7c6cf
Yumi Ta <soraishi08@gmail.com>
2019-08-23 08:04:50 -0400

    Add yell command.

--- cogs/fun.py
+++ cogs/fun.py
@@ -30,7 +30,12 @@ class Fun(commands.Cog):
 
     @commands.command(brief='Send an anonymous message to someone.')
     async def whisper(self, ctx, user : discord.User, *, message):
-        await user.send(f'Someone whispers to you: "{message}"')
+        await user.send(f'Someone whispers to you: "{message}".')
+        await ctx.message.delete()
+
+    @commands.command(brief='Send a not-so-anonymous message to someone.')
+    async def yell(self, ctx, user : discord.User, *, message):
+        await user.send(f'{ctx.author.mention} yells to you: "{message}".')
         await ctx.message.delete()
         
 
