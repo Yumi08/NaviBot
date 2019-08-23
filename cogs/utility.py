40f93a0ae8608d3805e8bacf9189de0f209b2c17
Yumi Ta <soraishi08@gmail.com>
2019-08-23 05:10:17 -0400

    Add info command.

--- cogs/utility.py
+++ cogs/utility.py
@@ -5,6 +5,11 @@ class Utility(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
+    @commands.command()
+    async def info(self, ctx):
+        await ctx.send('Navi v0.0.0, discord.py, created by Yumi.')
+
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'{round(self.client.latency * 1000)}ms')
@@ -23,7 +28,10 @@ class Utility(commands.Cog):
 
     @commands.command()
     async def status(self, ctx, *, status):
-        await self.client.change_presence(activity=discord.Game(status))
+        if ctx.author.id == 218429853144186883:
+            await self.client.change_presence(activity=discord.Game(status))
+        else:
+            await ctx.send('You\'re not Yumi.')
 
 def setup(client):
     client.add_cog(Utility(client))
